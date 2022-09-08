ace62bf2d73f2729f7f2b039ccde77e7b7db4117
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-09-08 19:55:30 +0530

    Create binaryTreeInorderTraversal.cpp

---
+++ binaryTreeInorderTraversal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        //binary tree inorder traversal using stack (not the auxillary stack space of recursion)
+        stack<TreeNode*> st;
+        TreeNode* node=root;
+        vector<int> inorder;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node=node->left;//call for left
+            }
+            else{
+                if(st.empty()) break;
+                node=st.top();
+                st.pop();
+                inorder.push_back(node->val);
+                node=node->right;//call for right
+            }
+        }
+        return inorder;
+    }
+};
