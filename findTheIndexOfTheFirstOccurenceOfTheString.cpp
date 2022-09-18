cb201ad9c05c5e537e73a31d752e24561fd25efa
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-09-18 13:12:54 +0530

    Create findTheIndexOfTheFirstOccurenceOfTheString.cpp

---
+++ findTheIndexOfTheFirstOccurenceOfTheString.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+		int needle_len = needle.length();
+		int haystack_len = haystack.length();
+		if(needle_len==0){
+			return 0;
+		}
+		int j = 0;
+		for(int i = 0 ; i < haystack_len ; i++){
+
+			if(haystack[i] == needle[j]){
+				j++;
+			}else{
+				i = i-j;
+				j = 0;
+			}
+			if(j==needle_len){
+				return i - j +1 ;
+			}
+
+		}
+		return -1;
+	}
+};
