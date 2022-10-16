a5a9eb232e0e47b822e7edf8b03f47011a74abe2
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-10-16 16:11:47 +0530

    Create sumOfNumbersAndItsReverse.cpp

---
+++ sumOfNumbersAndItsReverse.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        
+        for(int i =num/2;i<=num;i++)
+        {
+            int mid = i;
+            string s = to_string(mid);
+            reverse(s.begin(),s.end());
+            int n = stoi(s);
+            if((i+n)==num) return true;
+                
+        }
+        return false;
+        
+    }
+};
