9df11d47701a27ab540875f6859a86b48376c02f
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-11-08 00:12:23 +0530

    Create uniqueBinarySearchTrees.cpp

---
+++ uniqueBinarySearchTrees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int solve(int n,vector<int>& dp){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int ans=0;
+        for(int i=1;i<=n;i++){
+             ans+=solve(i-1,dp)*solve(n-i,dp);
+        }
+        
+        return dp[n]=ans;
+    }
+public:
+    int numTrees(int n) {
+        vector <int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
