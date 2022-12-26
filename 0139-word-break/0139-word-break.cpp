0507c16818799cf6727d3e728ef7f702d97fcb4e
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-12-26 22:49:27 +0530

    Time: 8 ms (78.40%), Space: 9.3 MB (67.85%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    int solve(string s,int idx,unordered_map<string,int>& mp,int dp[]){
+        
+        if(idx==s.size()) return 1;
+        if(dp[idx]!=-1) return dp[idx];
+        string temp;
+        for(int j=idx;j<s.size();j++){
+            temp+=s[j];
+            if(mp.find(temp)!=mp.end()){
+                if(solve(s,j+1,mp,dp)) return dp[idx]=1;
+            }
+        }
+        return dp[idx]=0;
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int dp[301];
+        memset(dp,-1,sizeof dp);
+        unordered_map<string,int> mp;
+        for(auto& idx:wordDict) mp[idx];
+        return solve(s,0,mp,dp);
+    }
+};
