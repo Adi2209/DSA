5746feb379c45630de4c508cd797c282a81e8bd4
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2022-12-30 12:51:23 +0530

    Time: 234 ms (66.12%), Space: 61.7 MB (21.51%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,43 +1,31 @@
 class Solution {
-private:
-    bool dfsCheck(int node,vector<vector<int>>& graph,vector<int>& vis,
-                  vector<int>& pathVis,vector<int>& check){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(auto it:graph[node]){
-            if(!vis[it]){
-                if(dfsCheck(it,graph,vis,pathVis,check)){
-                    check[node]=0;
-                    return true;
-                } 
-            }
-            else if(pathVis[it]){
-                check[node]=0;
-                return true;
-            }
-        }
-        check[node]=1;
-        pathVis[node]=0;
-        return false;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        //any one who is part of the cycle can't be a safe node
-        //any one leads to a a cycle
         int n=graph.size();
-        vector<int> vis(n,0);
-        vector<int> pathVis(n,0);
-        vector<int> safeNodes;
-        vector<int> check(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfsCheck(i,graph,vis,pathVis,check);
-            }
-        }
+        vector<vector<int>> adjRev(n);
+        vector<int> indegree(n);
         
         for(int i=0;i<n;i++){
-            if(check[i]==1) safeNodes.push_back(i);
+            for(auto it:graph[i]){
+                adjRev[it].push_back(i);
+                indegree[i]++;
+            }
         }
+        queue<int> q;
+        vector<int> safeNodes;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safeNodes.push_back(node);
+            for(auto it: adjRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        sort(safeNodes.begin(),safeNodes.end());
         return safeNodes;
     }
 };
