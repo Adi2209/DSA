699ae748b47e68e9f8eee33debfd7426bcf67831
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-01 18:57:18 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,39 +3,71 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet{
+    vector<int> rank,parent,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int findUPar(int node){
+        if(node==parent[node])
+            return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    
+    void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=parent[ulp_v];
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+    
+};
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        priority_queue<pair<int,int>,
-        vector<pair<int,int> >,
-        greater<pair<int,int>>> pq;
         
-        vector<int> vis(V,0);
-        //{wt,node}
-        pq.push({0,0});
-        int sum=0;
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int node=it.second;
-            int wt=it.first;
-            
-            if(vis[node]==1) continue;
-            //else add the node to the mst
-            vis[node]=1;
-            sum+=wt;
-            for(auto it:adj[node]){
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
                 int adjNode=it[0];
-                int edgeWt=it[1];
-                if(!vis[adjNode]){
-                    pq.push({edgeWt,adjNode});
-                }
+                int wt=it[1];
+                int node=i;
+                
+                edges.push_back({wt,{node,adjNode}});
             }
         }
-        return sum;
+        DisjointSet ds(V);
+        sort(edges.begin(),edges.end());
+        int mstwt=0;
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            if(ds.findUPar(u)!=ds.findUPar(v)){
+                mstwt+=wt;
+                ds.unionBySize(u,v);
+            }
+        }
+        return mstwt;
     }
 };
 
