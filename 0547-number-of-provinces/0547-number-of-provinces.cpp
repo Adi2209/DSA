73185e47c9016a5f21916810f288697ecabe4a42
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-01 19:35:02 +0530

    Time: 23 ms (92.33%), Space: 13.8 MB (56.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,37 +1,50 @@
-class Solution {
-private:
-    void dfs(int node,vector<int>& vis,vector<vector<int>> adjList){
-        
-        vis[node]=1;
-        
-        for(auto it: adjList[node]){
-            if(!vis[it])
-                dfs(it,vis,adjList);
+class DisjointSet{
+    public:
+    vector<int> rank,parent,size;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
     }
+    
+    int findUPar(int node){
+        if(node==parent[node])
+            return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    
+    void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=parent[ulp_v];
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+    
+};
+class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        //first we will make the adjList from the adjMatrix
         int n=isConnected.size();
-        vector<vector<int>> adjList(n);
-        
+        DisjointSet ds(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
+                if(isConnected[i][j]==1) ds.unionBySize(i,j);
             }
         }
-        
-        //now traversing the dfs
         int cnt=0;
-        vector<int> vis(n,0);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                cnt++;
-                dfs(i,vis,adjList); 
-            }
+            if(ds.parent[i]==i) cnt++;
         }
         return cnt;
     }
