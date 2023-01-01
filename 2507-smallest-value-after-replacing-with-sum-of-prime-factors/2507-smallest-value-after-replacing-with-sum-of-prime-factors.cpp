9f7b5a81761c55b75a123e48cc97b4c8d56c8e7f
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-01 13:25:17 +0530

    Time: 6 ms (49.40%), Space: 5.9 MB (72.91%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.cpp
@@ -1,26 +1,27 @@
 class Solution {
-private:
-    long long getSumOfFactors(int n){
-        int divisor=2;
-        long long ans=0;
-        
-        while(n>1){
-            if(n%divisor==0){
-                ans+=divisor;
-                n/=divisor;
+public:
+    long long getSumOfFactors(int n)
+    {
+        int divisor = 2;
+        long long ans = 0;
+        while(n > 1)
+        {
+            if (n % divisor == 0) 
+            {
+                ans += divisor;
+                n = n / divisor;
             }
-            else
-                divisor++;
+            else divisor++;
         }
         return ans;
     }
-public:
-    int smallestValue(int n) {
-        
-        while(true){
-            long long sumOfFactors=getSumOfFactors(n);
-            if(n==sumOfFactors) break;
-            n=sumOfFactors;
+    int smallestValue(int n) 
+    {   
+        while(true)
+        {
+            long long sumOfFactors = getSumOfFactors(n);
+            if (n == sumOfFactors) break;
+            n = sumOfFactors;
         }
         return n;
     }
