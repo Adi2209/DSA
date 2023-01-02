00ef60e48cea874b0e1029a9e2c5d604c8fe9c2c
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-02 21:35:34 +0530

    Create DisjoinSet.cpp

---
+++ DisjoinSet.cpp
@@ -0,0 +1,34 @@
+class DisjointSet{
+    public:
+    vector<int> rank,parent,size;
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    
+    int findUPar(int node){
+        if(node==parent[node])
+            return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+    
+    void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return ;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=parent[ulp_v];
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+    
+};
