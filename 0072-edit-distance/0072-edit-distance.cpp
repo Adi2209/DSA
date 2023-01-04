c4c95900af30ac16e4f3225304c13db2124a7caf
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-05 01:57:21 +0530

    Time: 41 ms (27.06%), Space: 9.1 MB (22.97%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,35 +1,40 @@
 class Solution {
 private:
-    int solveRec(string word1, string word2,int size1,int size2,int ind1,int ind2,vector<vector<int> >& dp){
-        //base case
-        if(ind1==size1){
-            return size2-ind2;
+    int solveTab(string word1,string word2,int size1,int size2){
+        
+        vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
+        
+        for(int ind=0;ind<size2;ind++){
+            dp[size1][ind]=size2-ind;
         }
-        if(ind2==size2){
-            return size1-ind1;
+        for(int ind=0;ind<size1;ind++){
+            dp[ind][size2]=size1-ind;
         }
-        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
-        int minSteps=0;
-        if(word1[ind1]==word2[ind2]){
-            return solveRec(word1,word2,size1,size2,ind1+1,ind2+1,dp);
+        for(int ind1=size1-1;ind1>=0;ind1--){
+            for(int ind2=size2-1;ind2>=0;ind2--){
+                int minSteps=0;
+                if(word1[ind1]==word2[ind2]){
+                    minSteps=dp[ind1+1][ind2+1];
+                }
+                else{
+                    //insert
+                    int insertCh=1+dp[ind1][ind2+1];
+                    //delete
+                    int deleteCh=1+dp[ind1+1][ind2];
+                    //replace
+                    int replaceCh=1+dp[ind1+1][ind2+1];
+
+                    minSteps=min(insertCh,min(deleteCh,replaceCh));
+                }
+                 dp[ind1][ind2]=minSteps;
+            }
         }
-        else{
-            //insert
-            int insertCh=1+solveRec(word1,word2,size1,size2,ind1,ind2+1,dp);
-            //delete
-            int deleteCh=1+solveRec(word1,word2,size1,size2,ind1+1,ind2,dp);
-            //replace
-            int replaceCh=1+solveRec(word1,word2,size1,size2,ind1+1,ind2+1,dp);
-                
-            minSteps=min(insertCh,min(deleteCh,replaceCh));
-        }
-        return dp[ind1][ind2]=minSteps;
+        return dp[0][0];
     }
 public:
     int minDistance(string word1, string word2) {
         int size1=word1.size();
         int size2=word2.size();
-        vector<vector<int>> dp(size1,vector<int>(size2,-1));
-        return solveRec(word1,word2,size1,size2,0,0,dp);
+        return solveTab(word1,word2,size1,size2);
     }
 };
