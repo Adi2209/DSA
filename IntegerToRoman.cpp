fca89d8daddc1783907341b64ad28e7012a2940a
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-10 08:21:51 +0530

    Update IntegerToRoman.cpp

--- IntegerToRoman.cpp
+++ IntegerToRoman.cpp
@@ -2,7 +2,7 @@ class Solution{
 public:
     string intToRoman(int num) {
         string ans;
-        
+        //storing all the roman numbers and their values
         int number[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string roman[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
