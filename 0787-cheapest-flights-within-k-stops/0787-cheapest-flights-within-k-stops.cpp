b7bc62ee56f8a5ee4dd940c1ab1ce01f7415f9c2
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-27 00:00:37 +0530

    Time: 37 ms (52.40%), Space: 13.6 MB (52.92%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,43 @@
+class flight{
+    public:
+    int stops,node,dist;
+    flight(int stops,int node,int dist){
+        this->stops=stops;
+        this->node=node;
+        this->dist=dist;
+    }
+    
+};
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int,int>> > adj(n);
+        vector<vector<pair<int,int>>> adj(n);
         
         for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        queue<pair<int,pair<int,int>>> q;
-        // {stops,{src node,cost}}
-        q.push({0,{src,0}});
+        queue<flight> que;
+        que.push({0,src,0});
         vector<int> dist(n,1e9);
         dist[src]=0;
-        while(!q.empty()){
-            auto it=q.front();
-            q.pop();
-            int stops=it.first;
-            int node=it.second.first;
-            int cost=it.second.second;
-            
+        
+        while(!que.empty()){
+            int stops=que.front().stops;
+            int node=que.front().node;
+            int cost=que.front().dist;
+            que.pop();
             if(stops>k) continue;
             for(auto it:adj[node]){
                 int adjNode=it.first;
-                int edw=it.second;
-                
-                if(cost + edw <dist[adjNode] && stops<=k){
-                    dist[adjNode]=cost+edw;
-                    q.push({stops+1,{adjNode,cost+edw}});
+                int edgeDist=it.second;
+                int newCost=cost + edgeDist;
+                if(newCost <dist[adjNode] && stops<=k){
+                    dist[adjNode]=newCost;
+                    que.push({stops+1,adjNode,newCost});
                 }
             }
-            
-        }   
+        }
+        
         if(dist[dst]==1e9) return -1;
         return dist[dst];
         
