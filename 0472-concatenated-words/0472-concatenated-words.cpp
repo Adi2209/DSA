a5610a4d0f155dbcb1cc8d0b266be5d89d0a7770
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-27 23:49:23 +0530

    Time: 149 ms (98.88%), Space: 40.3 MB (87.34%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,21 +1,30 @@
 class Solution {
-public:
-    set<string> s;
-    bool checkConcatenate(string word) {
-        for(int i = 1; i < word.length(); i++) {
+private:
+    bool checkConcatenate(string word,unordered_set<string>& st,int minLen,set<string>& tempCache) {
+        if(tempCache.find(word)!=tempCache.end()) return true;
+        for(int i = minLen; i <= word.length()-minLen; i++) {
             string prefixWord = word.substr(0, i);
             string suffixWord = word.substr(i, word.length()-i);
-            if(s.find(prefixWord) != s.end() && (s.find(suffixWord) != s.end() || checkConcatenate(suffixWord)))
+            if(st.find(prefixWord) != st.end() && (st.find(suffixWord) != st.end() || checkConcatenate(suffixWord,st,minLen,tempCache))){
+                tempCache.insert(word);
                 return true;
+            }
         }
         return false;
     }
+public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         vector<string> concatenateWords;
-        for(string word : words)
-            s.insert(word);
-        for(string word : words) {
-            if(checkConcatenate(word) == true)
+        unordered_set<string> st; 
+        set<string> tempCache;
+        int minLen=1e9;
+        for(auto word : words){
+            st.insert(word);
+            int size=word.size();
+            minLen=min(minLen,size);
+        }
+        for(auto word : words) {
+            if(checkConcatenate(word,st,minLen,tempCache))
                 concatenateWords.push_back(word);
         }
         return concatenateWords;
