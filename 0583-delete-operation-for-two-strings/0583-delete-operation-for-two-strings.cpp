eeb7ecf54597ffacd84edb6e7b1cf2034d325f01
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-01-31 00:59:02 +0530

    Time: 24 ms (63.98%), Space: 12.3 MB (17.73%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,18 +1,5 @@
 class Solution {
 private:
-    int lcs(int i,int j,string &word1,string& word2,int size1,int size2){
-        if(i==size1 || j==size2) return 0;
-        int ans=0;
-        //solving for one case
-        if(word1[i]==word2[j]){
-            ans=1+lcs(i+1,j+1,word1,word2,size1,size2);
-        }
-        else{
-            ans=max(lcs(i+1,j,word1,word2,size1,size2),lcs(i,j+1,word1,word2,size1,size2));
-        }
-        return ans;
-    }
-    
     int lcs(string& word1,string& word2,int size1,int size2){
         vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
         
