4dc4ed0a6b070998a9c8d490e8079c0fd32440b1
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-02-08 23:04:27 +0530

    Time: 533 ms (19.64%), Space: 18 MB (9.98%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 private:
-    int solveRec(int ind, vector<int>& nums,vector<int>& dp){
+    int solveRec(int ind,vector<int>& nums,vector<int>& dp){
         if(ind>=nums.size()-1) return 0;
         if(dp[ind]!=-1) return dp[ind];
+        int steps=nums.size();//max steps to reach the dest
         //take
-        int steps=nums.size();
-        for(int i=ind+1; i<=ind+nums[ind]; i++){
-            steps=min(steps, 1+solveRec(i, nums,dp));
+        for(int i=ind+1;i<=ind+nums[ind];i++){
+            steps=min(steps,1+solveRec(i,nums,dp));
         }
         return dp[ind]=steps;
     }
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size()+1,-1);
+        int n=nums.size();
+        vector<int> dp(n+1,-1);
         return solveRec(0,nums,dp);
     }
 };
