df3f7cb3a02c3a8ee02213713cff8b3c927b9fcf
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-02-14 23:44:22 +0530

    Time: 93 ms (17.13%), Space: 19.9 MB (11.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,35 +1,31 @@
 class Solution {
 private:
     bool isValid(int nRow,int nCol,int rowSize,int colSize){
-        return nRow>=0  && nRow<rowSize && nCol>=0 && nCol<colSize;
+        return nRow<rowSize && nCol<colSize && nRow>=0 & nCol>=0;
     }
-    void markIsland(int row,int col,vector<vector<char>>& grid,int rowSize,int colSize,vector<vector<char>> &vis){
-        char island='1';
+    void markIsland(int row,int col,vector<vector<char>>& vis,int rowSize,int colSize){
         vis[row][col]='2';
-        
+        char island='1';
         vector<int> delRow={-1,0,1,0};
         vector<int> delCol={0,1,0,-1};
-        
         for(int ind=0;ind<4;ind++){
             int nRow=row+delRow[ind];
             int nCol=col+delCol[ind];
             if(isValid(nRow,nCol,rowSize,colSize) && vis[nRow][nCol]==island){
-                markIsland(nRow,nCol,grid,rowSize,colSize,vis);
+                markIsland(nRow,nCol,vis,rowSize,colSize);
             }
         }
     }
 public:
     int numIslands(vector<vector<char>>& grid) {
         int rowSize=grid.size(),colSize=grid[0].size();
+        vector<vector<char>>vis=grid;
         int islandCnt=0;
-        vector<vector<char>> vis(rowSize,vector<char>(colSize));
-        vis=grid;
-        //for marking purpose
         char island='1';
         for(int row=0;row<rowSize;row++){
             for(int col=0;col<colSize;col++){
                 if(vis[row][col]==island){
-                    markIsland(row,col,grid,rowSize,colSize,vis);
+                    markIsland(row,col,vis,rowSize,colSize);
                     islandCnt++;
                 }
             }
