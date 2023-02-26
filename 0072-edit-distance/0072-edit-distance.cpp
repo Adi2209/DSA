d4a98dc964c50c76fcfaa72f7f5f66ada0035fb2
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-02-26 14:15:21 +0530

    Time: 10 ms (80.23%), Space: 6.5 MB (93.87%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,39 +2,47 @@ class Solution {
 private:
     int solveTab(string word1,string word2,int size1,int size2){
         
-        vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
+        // vector<vector<int>> dp(size1+1,vector<int>(size2+1,0));
+        
+        vector<int> curr(size2+1,0);
+        vector<int> next(size2+1,0);
         
         for(int ind=0;ind<size2;ind++){
-            dp[size1][ind]=size2-ind;
+            next[ind]=size2-ind;
         }
-        for(int ind=0;ind<size1;ind++){
-            dp[ind][size2]=size1-ind;
-        }
+  
         for(int ind1=size1-1;ind1>=0;ind1--){
             for(int ind2=size2-1;ind2>=0;ind2--){
+                //base case here-->
+                curr[size2]=size1-ind1;
+                
                 int minSteps=0;
+                
                 if(word1[ind1]==word2[ind2]){
-                    minSteps=dp[ind1+1][ind2+1];
+                    minSteps=next[ind2+1];
                 }
                 else{
                     //insert
-                    int insertCh=1+dp[ind1][ind2+1];
+                    int insertCh=1+curr[ind2+1];
                     //delete
-                    int deleteCh=1+dp[ind1+1][ind2];
+                    int deleteCh=1+next[ind2];
                     //replace
-                    int replaceCh=1+dp[ind1+1][ind2+1];
+                    int replaceCh=1+next[ind2+1];
 
                     minSteps=min(insertCh,min(deleteCh,replaceCh));
                 }
-                 dp[ind1][ind2]=minSteps;
+                 curr[ind2]=minSteps;
             }
+            next=curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 public:
     int minDistance(string word1, string word2) {
         int size1=word1.size();
         int size2=word2.size();
+        if(size1==0) return size2;
+        if(size2==0) return size1;
         return solveTab(word1,word2,size1,size2);
     }
 };
