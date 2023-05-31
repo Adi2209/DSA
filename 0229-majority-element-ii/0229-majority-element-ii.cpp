a00193c14581ddd6c07ec87d6c0a36a3f512cb69
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-05-31 17:06:29 +0530

    Time: 18 ms (33.85%), Space: 15.9 MB (75.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,12 +1,37 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        // this can be solved using moore's voting algo with some modifications
         int n=nums.size();
-        int k=n/3;
+        int ele1=0,ele2=0,cnt1=0,cnt2=0;
+        
+        for(int ind=0;ind<n;ind++){
+            if(cnt1==0 && ele2!=nums[ind]){
+                cnt1=1;
+                ele1=nums[ind];
+            }
+            else if(cnt2==0 && ele1!=nums[ind]){
+                cnt2=1;
+                ele2=nums[ind];
+            }
+            else if(ele1==nums[ind]) cnt1++;
+            else if(ele2==nums[ind]) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+        cnt1=0,cnt2=0;
+        for(auto &it:nums){
+            if(it==ele1) cnt1++;
+            else if(it==ele2) cnt2++;
+        }
         vector<int> ans;
-        unordered_map<int,int> mp;
-        for(auto &i:nums) mp[i]++;
-        for(auto &i:mp) if(i.second>k) ans.push_back(i.first);
+        int mini=(int)(n/3)+1;
+        if(cnt1>=mini) ans.push_back(ele1);
+        if(cnt2>=mini) ans.push_back(ele2);
+        
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
