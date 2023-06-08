cffeaa34cd2835ccd05ca1fd616ee62223ae033e
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-09 00:54:16 +0530

    Time: 15 ms (78.23%), Space: 19.8 MB (76.28%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int rightMax=-1,leftMax=-1,n=height.size();
-        int left=0,right=n-1;
-        int ans;
+        int n=height.size();
+        int left=0, right=n-1;
+        int ans=0,leftMax=-1,rightMax=-1;
         
-        while(left<=right){
+        while(left<right){
+            // this will check automatically the available right building to store water
             if(height[left]<=height[right]){
-                if(height[left]>=leftMax) leftMax=height[left];
+                //if it is the leftMax then it can't store water
+                if(height[left]>leftMax) leftMax=height[left];
                 else ans+=leftMax-height[left];
                 left++;
             }
             else{
-                //height[right]>height[left]
-                if(height[right]>=rightMax) rightMax=height[right];
+                //height[left]>height[right]
+                if(height[right]>rightMax) rightMax=height[right];
                 else ans+=rightMax-height[right];
                 right--;
             }
