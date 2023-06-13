64f95200186f35014b0273453bb526153d462628
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-14 00:20:05 +0530

    Time: 3 ms (93.09%), Space: 10.6 MB (99.00%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 private:
-    void solve(vector<int>& candidates,int idx,int target,vector<vector<int>>& ans,vector<int>& res){
+    void solve(int ind,int target,vector<vector<int>>& ans,vector<int>& res, vector<int>& candidates,int n){
         
-        //base case
         if(target==0){
             ans.push_back(res);
-            return;
-        }
-        for(int i=idx;i<candidates.size();i++){
-            if(candidates[i]<=target){
-                res.push_back(candidates[i]);
-                solve(candidates,i,target-candidates[i],ans,res);
-                res.pop_back();
-            }
-            
+            return ;
         }
         
+        for(int i=ind;i<n;i++){
+            if(candidates[i]<=target){
+                res.push_back(candidates[i]);
+                solve(i,target-candidates[i],ans,res,candidates,n);
+                res.pop_back();
+            }
+        }
     }
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> res;
-        solve(candidates,0,target,ans,res);
+        int n=candidates.size();
+        solve(0,target,ans,res,candidates,n);
         return ans;
     }
 };
