bd012943b25f3f1255ec43b4a60d491bd65a6a7f
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-14 01:25:34 +0530

    Time: 4 ms (75.65%), Space: 10.5 MB (82.44%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,26 +1,25 @@
 class Solution {
 private:
-    void solve(int idx,int target,vector<int>& arr,vector<vector<int>>& ans,vector<int>& current){
-        //base case
-            if(target==0){
-                ans.push_back(current);
-                return;
-            }
-        
-        for(int i=idx;i<arr.size();i++){
-            if(i>idx && arr[i]==arr[i-1]) continue;
-            if(arr[i]>target) break;
-            current.push_back(arr[i]);
-            solve(i+1,target-arr[i],arr,ans,current);
-            current.pop_back();
+    void solve(int ind, int target, vector<vector<int>>& ans,vector<int>& res, int n,vector<int>& candidates ){
+        if(target==0){
+            ans.push_back(res);
+            return;
+        }
+        for(int i=ind;i<n;i++){
+            if(i!=ind && candidates[i]==candidates[i-1]) continue;
+            if(candidates[i]>target) continue;
+            res.push_back(candidates[i]);
+            solve(i+1,target-candidates[i],ans,res,n,candidates);
+            res.pop_back();
         }
     }
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());//sort the given candidates vector 
         vector<vector<int>> ans;
         vector<int> res;
-        solve(0,target,candidates,ans,res);
+        int n=candidates.size();
+        sort(candidates.begin(),candidates.end());
+        solve(0,target,ans,res,n,candidates);
         return ans;
     }
 };
