2b177a8c4cb0ede1e8ca4dab0430c2135d4d9143
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-16 00:54:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (7.04%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -11,8 +11,8 @@ public:
         string ans="";
         k--;
         while(true){
-            ans=ans+to_string(numbers[k/fact]);
-            numbers.erase(numbers.begin()+k/fact);
+            ans=ans+to_string(numbers[k/fact]);//number which will form the next digit will be added to the ans
+            numbers.erase(numbers.begin()+k/fact);//erase the number which is processed
             if(numbers.size()==0) break;
             k=k%fact;
             fact=fact/numbers.size();
