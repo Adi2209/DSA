bdab5036a6a6f0e16740af716cca46948e83c920
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-29 01:00:16 +0530

    Time: 5 ms (46.94%), Space: 11 MB (95.59%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -5,20 +5,18 @@ public:
         int low=0,high=n-1;
         
         while(low<=high){
-            int mid=(low+high)>>1;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            //if the left side is sorted 
-            else if(nums[low]<=nums[mid]){
-                //if in the left sorted part
-                if(nums[low]<= target && target<nums[mid]){
+            if(nums[low]<=nums[mid]){
+                //left is sorted
+                if(nums[low]<=target && target<nums[mid]){
                     high=mid-1;
                 }
                 else low=mid+1;
             }
-            // if right side is sorted
             else{
-                //if in the right sorted part
-                if(nums[mid]<target && nums[high]>=target){
+                //right sorted
+                if(nums[mid]<=target && target<=nums[high]){
                     low=mid+1;
                 }
                 else high=mid-1;
