60a275abdc89b17a4b2bfa37f285832c14b7f413
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-29 22:00:58 +0530

    Time: 54 ms (58.78%), Space: 18.9 MB (37.98%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,40 +1,35 @@
 class Solution {
-    
-    bool isPossible(vector<int>& piles, int k,int h){
-
-        long long hours = 0;
-        for(int pile : piles){
-            long long div = pile / k;
-            hours += div;
-            if(pile % k != 0) hours++;
+private:
+    int getMax(vector<int>& piles){
+        int maxi=-1;
+        for(auto &it:piles){
+            maxi=max(it,maxi);
         }
-        return hours <= h;
+        return maxi;
+    }
+    bool canEat(int k,int h,vector<int>& piles){
+        long long hours=0;
+        for(auto pile:piles){
+            long long div=pile/k;
+            hours+=div;
+            if(pile%k!=0) hours++;
+        }
+        return hours<=h;
     }
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        
         int n=piles.size();
-        int s=1;
-        int maxi=0;
-        
-        for(int i=0;i<n;i++){
-            if(maxi<piles[i]) maxi=piles[i];
-                
-        }
-        int e=maxi;
-        // cout<<e<<endl;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            
-            if(isPossible(piles,mid,h)){
-                e=mid-1;
+        int low=1;
+        int high= getMax(piles);
+        int ans=high;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(canEat(mid,h,piles)){
+                ans=mid;
+                high=mid-1;
             }
-            else{
-                s=mid+1;
-            }
+            else low=mid+1;
         }
-        
-        return s;
-        
+        return ans;
     }
 };
