116f99f3db9f48655a0a3c71aa87c5f692c2495e
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2023-06-30 21:53:38 +0530

    Time: 12 ms (6.17%), Space: 12 MB (62.25%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,25 +5,21 @@ public:
         int breakpoint=-1;
         for(int ind=n-2;ind>=0;ind--){
             if(nums[ind+1]>nums[ind]){
-                //breakpoint found
                 breakpoint=ind;
                 break;
             }
         }
-        //now if no breakpoint found this means that the array is sorted in decreasing order
-        //so now we will reverse the array and return it
         if(breakpoint==-1){
             reverse(nums.begin(),nums.end());
+            return;
         }
-        else{
-            //if breakpoint found, now find the element greater to the arr[ind] on the right half
-            for(int ind=n-1;ind>breakpoint;ind--){
-                if(nums[breakpoint]<nums[ind]){
-                    swap(nums[breakpoint],nums[ind]);
-                    reverse(nums.begin()+breakpoint+1,nums.end());
-                    break;
-                }
+        
+        for(int ind=n-1;ind>breakpoint;ind--){
+            if(nums[ind]>nums[breakpoint]){
+                swap(nums[ind],nums[breakpoint]);
+                reverse(nums.begin()+breakpoint+1,nums.end());
+                break;
             }
-        }        
+        }
     }
 };
