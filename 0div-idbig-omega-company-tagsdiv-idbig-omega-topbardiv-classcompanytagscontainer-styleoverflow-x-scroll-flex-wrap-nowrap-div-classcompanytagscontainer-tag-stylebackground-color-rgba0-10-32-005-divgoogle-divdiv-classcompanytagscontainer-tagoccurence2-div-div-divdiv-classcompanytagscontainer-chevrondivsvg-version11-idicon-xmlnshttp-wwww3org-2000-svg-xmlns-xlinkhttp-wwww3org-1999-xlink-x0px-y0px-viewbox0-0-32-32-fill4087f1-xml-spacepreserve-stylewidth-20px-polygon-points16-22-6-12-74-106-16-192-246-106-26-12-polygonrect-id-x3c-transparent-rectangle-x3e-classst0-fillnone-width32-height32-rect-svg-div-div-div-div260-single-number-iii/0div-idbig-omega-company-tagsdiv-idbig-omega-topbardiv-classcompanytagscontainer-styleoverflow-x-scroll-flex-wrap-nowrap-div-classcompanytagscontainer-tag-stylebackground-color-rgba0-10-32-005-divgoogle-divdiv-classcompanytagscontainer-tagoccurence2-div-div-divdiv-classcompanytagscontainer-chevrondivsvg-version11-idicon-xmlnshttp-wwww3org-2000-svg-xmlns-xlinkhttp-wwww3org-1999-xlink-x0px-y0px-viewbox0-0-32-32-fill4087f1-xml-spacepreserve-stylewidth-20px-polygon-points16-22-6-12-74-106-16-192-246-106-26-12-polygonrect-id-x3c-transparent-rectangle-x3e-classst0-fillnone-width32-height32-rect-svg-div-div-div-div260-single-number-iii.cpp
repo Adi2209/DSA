b2256257110d0b3a7bf3443976ac08c1909396c3
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2024-05-31 10:32:15 +0530

    Time: 7 ms (62.82%), Space: 12.6 MB (38.03%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div260-single-number-iii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div260-single-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        sort (nums.begin(), nums.end());
+	vector<int> ans;
+	int i = 0, n = nums.size();
+	while (i < n - 1)
+	    // skip equal elements
+		if (nums[i] == nums[i + 1]) i += 2;
+		// generate ans
+		else {
+			ans.push_back(nums[i++]);
+			if (ans.size() == 2)
+				return ans;
+		}
+	// corner case
+	if (ans.size() == 1) 
+		ans.push_back(nums.back());
+	return ans;
+    }
+};
