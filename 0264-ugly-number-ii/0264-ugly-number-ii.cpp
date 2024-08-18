cdb05f221df04d19b9303317a8ee18fbe21d47fb
Aditya Nigam <75134530+Adi2209@users.noreply.github.com>
2024-08-18 23:26:14 +0530

    Time: 75 ms (29.00%), Space: 31.8 MB (26.05%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        set<long> st;
+        long num=n;
+        st.insert(1);
+        
+        for(int i=0;i<n;i++){
+            num=*st.begin();
+            st.erase(num);
+            st.insert(num*2);
+            st.insert(num*3);
+            st.insert(num*5);
+        }
+        return num;
+    }
+};
